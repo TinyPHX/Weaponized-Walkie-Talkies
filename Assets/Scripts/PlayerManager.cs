1aba8d18fedf7bdbe98651562236b382cb8c7408
popwser <michaelbuergerart@gmail.com>
2018-01-27 15:59:56 -0600

    PlayerManager stuff done

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-class PlayerManager
+class PlayerManager : MonoBehaviour
 {
-    List<PlayerController> playerControllers = new List<PlayerController> { };
-    List<InputDevice> allInputDevices = new List<InputDevice> { };
-    List<InputDevice.ID> unassignedInputDevices = new List<InputDevice.ID> { };
+    public List<PlayerController> playerControllers = new List<PlayerController> { };
+    public List<InputDevice> allInputDevices = new List<InputDevice> { };
+    public List<InputDevice.ID> unassignedInputDevices = new List<InputDevice.ID> { };
 
     public void Start()
     {
@@ -15,6 +15,7 @@ class PlayerManager
         {
             InputDevice.ID inputDeviceID = InputDevice.CONTROLLERS[i];
             InputDevice inputDevice = new InputDevice(inputDeviceID);
+            unassignedInputDevices.Add(inputDeviceID);
 
             allInputDevices.Add(inputDevice);
         }
@@ -22,23 +23,49 @@ class PlayerManager
 
     public void Update()
     {
-
+        UpdateInputDeviceAssignments();
+        RefreshInputDevices();
     }
     
-    private void UpdateInputDeviceAssignments(PlayerController playerController)
+    private void UpdateInputDeviceAssignments()
     {
         //for each playerController 
             //if playerController.inputDevice == null
                 //loop each allInputDevices
                     //check if "A" pressed
                         //playerController.inputDevice = curentInputDevice
+        foreach (PlayerController pc in playerControllers) {
+            if(pc.inputDevice == null)
+            {
+                foreach (InputDevice.ID i in unassignedInputDevices)
+                {
+                    foreach (InputDevice inputDevice in allInputDevices) {
+                        if (inputDevice != null && inputDevice.Id == i && inputDevice.GetAxis(InputDevice.GenericInputs.ACTION_1) > 0)
+                        {
+                            pc.inputDevice = inputDevice;
+                            unassignedInputDevices.Remove(pc.inputDevice.Id);
+                        }
+                    }
+                }
+            }
+
+        }
     }
 
     private void RefreshInputDevices()
     {
         //loop through playerControllers
-            //check if playerController.inputDevice.IsController() == false
-                //playerController.InputDevice = null;
+        //check if playerController.inputDevice.IsController() == false
+        //playerController.InputDevice = null;
+        foreach (PlayerController pc in playerControllers)
+        {
+            if (pc.inputDevice != null && pc.inputDevice.IsController() == false)
+            {
+                unassignedInputDevices.Add(pc.inputDevice.Id);
+                pc.inputDevice = null;
+            }
+
+        }
     }
 
 }
