4cb9c42d0e88c0dff3d4ad2ee8ff77eb4e091e58
popwser <michaelbuergerart@gmail.com>
2018-01-26 22:38:20 -0600

    Added more structure
    Yep

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float move_speed = 10f; // units per second
+    public float rotate_speed = 10f; // degrees per second
+    public string horizontal_position_axis = "WASD Horizontal";
+    public string vertical_position_axis = "WASD Vertical";
+    public string horizontal_rotation_axis = "Arrow Keys Horizontal";
+    public string vertical_rotation_axis = "Arrow Keys Vertical";
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position.Set(Input.GetKey * Time.deltaTime * move_speed, 0f, (transform.position.z + Input.GetAxis(vertical_position_axis)) * Time.deltaTime * move_speed); // set position with wasd
+
+	}
+}
