0dfadf52a6fe6c03a119855d8ab947e965e3a2e6
mkatic <michaelkatic@gmail.com>
2018-01-27 15:21:56 -0700

    Template code for managing controllers added.

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PlayerManager
+{
+    List<PlayerController> playerControllers = new List<PlayerController> { };
+    List<InputDevice> allInputDevices = new List<InputDevice> { };
+    List<InputDevice.ID> unassignedInputDevices = new List<InputDevice.ID> { };
+
+    public void Start()
+    {
+        int controllerCount = InputDevice.CONTROLLERS.Count;
+        for (int i = 0; i < controllerCount; i++)
+        {
+            InputDevice.ID inputDeviceID = InputDevice.CONTROLLERS[i];
+            InputDevice inputDevice = new InputDevice(inputDeviceID);
+
+            allInputDevices.Add(inputDevice);
+        }
+    }
+
+    public void Update()
+    {
+
+    }
+    
+    private void UpdateInputDeviceAssignments(PlayerController playerController)
+    {
+        //for each playerController 
+            //if playerController.inputDevice == null
+                //loop each allInputDevices
+                    //check if "A" pressed
+                        //playerController.inputDevice = curentInputDevice
+    }
+
+    private void RefreshInputDevices()
+    {
+        //loop through playerControllers
+            //check if playerController.inputDevice.IsController() == false
+                //playerController.InputDevice = null;
+    }
+
+}
